f3a01f7547be15b20191e2a4c54ba8c749f692af
Matheusdnf <matheusdiniz870@gmail.com>
2024-07-02 11:35:21 -0300

    controlador de conexão wifi

---
+++ lib/controler/network_controller.dart
@@ -0,0 +1,41 @@
+import 'package:connectivity/connectivity.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+
+class NewtworkController extends GetxController {
+  final Connectivity _connectivity = Connectivity();
+  @override
+  void onInit() {
+    super.onInit();
+    _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
+  }
+
+  void _updateConnectionStatus(ConnectivityResult connectivityResult) {
+    if (connectivityResult == ConnectivityResult.none) {
+      Get.rawSnackbar(
+          messageText: Text(
+            'Por favor Conectese a internet ! ',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 15,
+            ),
+          ),
+          isDismissible: false,
+          duration: const Duration(days: 1),
+          backgroundColor: Colors.red[400]!,
+          icon: const Icon(
+            Icons.wifi_off,
+            color: Colors.white,
+            size: 35,
+          ),
+          margin: EdgeInsets.zero,
+          snackStyle: SnackStyle.GROUNDED);
+    } else {
+      if (Get.isSnackbarOpen) {
+        Get.closeAllSnackbars();
+      }
+    }
+  }
+}
