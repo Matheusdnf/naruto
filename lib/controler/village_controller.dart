7c530a89c819231abe71666209de847f77ef6010
Matheus Diniz <142422460+Matheusdnf@users.noreply.github.com>
2024-07-22 18:08:03 -0300

    arquivos de vila,controller,model e repostitory

--- lib/controler/village_controller.dart
+++ lib/controler/village_controller.dart
@@ -1,13 +1,18 @@
 import 'package:get/get.dart';
+import 'package:teste/models/model_vila.dart';
+import 'package:teste/repository/repository_vila.dart';
 
 class VillageController extends GetxController {
-  var villages = <String>[].obs; // Lista de nomes das vilas
-  var isLoading = false.obs;
+  var villages = <String>[].obs;
+  var isLoading = true.obs;
+  var vila = <Vila>[].obs;
+  var selectedVillageId = 0.obs;
+
+  final Vilareposity vilareposity = Vilareposity(); // Instância do repositório
 
   @override
   void onInit() {
     super.onInit();
-    // Defina as vilas específicas aqui
     villages.value = [
       'Konohagakure',
       'Sunagakure',
@@ -15,6 +20,19 @@ class VillageController extends GetxController {
       'Iwagakure',
       'Kumogakure',
     ];
-    isLoading.value = false; // Atualize conforme necessário
+    isLoading.value = false;
+  }
+
+  void fetchVila(int villageId) async {
+    try {
+      isLoading(true);
+      selectedVillageId.value = villageId;
+      final List<Vila> fetchedVila = await vilareposity.getVila(villageId);
+      vila.assignAll(fetchedVila);
+    } catch (e) {
+      print('Erro ao buscar personagens: $vila');
+    } finally {
+      isLoading(false);
+    }
   }
 }
---
+++ lib/repository/repository_vila.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:teste/models/model_vila.dart';
+
+abstract class IVilareposity {
+  Future<List<Vila>> getVila(int escolha);
+}
+
+class Vilareposity implements IVilareposity {
+  @override
+  Future<List<Vila>> getVila(int escolha) async {
+    final url = 'https://narutodb.xyz/api/village/$escolha';
+
+    try {
+      final response = await http.get(Uri.parse(url));
+
+      print('Status Code: ${response.statusCode}');
+      print('Response Body: ${response.body}');
+
+      if (response.statusCode == 200) {
+        final dynamic jsonBody = jsonDecode(response.body);
+        final List<dynamic> vilaList = jsonBody['villages'] ?? [];
+        List<Vila> vila = vilaList.map((json) => Vila.fromJson(json)).toList();
+        return vila;
+      } else if (response.statusCode == 404) {
+        throw Exception("Não encontrado");
+      } else {
+        throw Exception('Falha ao carregar os personagens');
+      }
+    } catch (e) {
+      print('Erro na requisição: $e');
+      throw e;
+    }
+  }
+}
