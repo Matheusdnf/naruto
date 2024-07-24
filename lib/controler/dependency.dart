f22b0e5e1194321777ec69d70735751ffa617097
Matheus Diniz <142422460+Matheusdnf@users.noreply.github.com>
2024-07-24 14:03:27 -0300

    organização do projeto (remoção de arquivos e renomeiação)

--- lib/controler/dependency.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-import 'package:teste/controler/network_controller.dart';
-
-class DependencyInjection {
-  static void init() {
-    Get.put<NewtworkController>(NewtworkController(), permanent: true);
-  }
-}
