ac5a45b387eca14d4e2a6bcb266cddd294376ced
Matheus Diniz <142422460+Matheusdnf@users.noreply.github.com>
2024-08-02 15:42:18 -0300

    atualização do controlador da barra de navegação (navbar)

--- lib/controller/controller_navigationbar.dart
+++ lib/controller/controller_navigationbar.dart
@@ -1,26 +1,19 @@
-// import 'package:get/get.dart';
-// import 'package:teste/scream/home_scream.dart';
-// class BottomNavController extends GetxController {
-//   //observar a mudança do index (toque no botão)
-//   var currentIndex = 0.obs;
-//   //realizar a mudança de págia 
-//   void changePage(int index) {
-//     //pega a mudança do index e atrela a uma varíavel
-//     currentIndex.value = index;
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
 
-//     switch (index) {
-//       case 0:
-//         Get.to(MyHomePage());
-//         break;
-//       case 1:
-//         print("outro botão ");
-//         break;
-//       case 2:
-//         // Lógica de navegação para a página de Grupos
-//         print("Terceiro botão ");
-//         break;
-//       default:
-//         Get.toNamed('/'); // Página padrão
-//     }
-//   }
-// }
+class NavController extends GetxController {
+  var currentIndex = 0.obs;
+  final PageController pageController = PageController();
+
+  void changePage(int index) {
+    currentIndex.value = index;
+    //mudar instataneamente para a página 
+    pageController.jumpToPage(index);
+  }
+  //descartar o controlar quando não for mais necessário 
+  @override
+  void onClose() {
+    pageController.dispose();
+    super.onClose();
+  }
+}
