df2703b8d83a85a30fad9b174d5e1eb71dccad81
Matheus Diniz <142422460+Matheusdnf@users.noreply.github.com>
2024-08-11 22:39:19 -0300

    novas tela de inicio e vila

---
+++ lib/screens/screen_routes/tela_inicio.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:teste/models/models_widget/models_widgets.dart';
+import 'package:teste/screens/screens_info_app/about.dart';
+import 'package:teste/screens/screens_info_app/logo_app.dart';
+
+class FirstScream extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Naruto Universe',
+      home: Scaffold(
+        backgroundColor: Colors.orange,
+        body: SafeArea(
+          child: Stack(
+            children: [
+              Positioned(
+                top: 0,
+                right: 0,
+                child: MyButton(
+                  modelo: [
+                    ModelButton(
+                      nome: 'Desenvolvedores',
+                      icon: const Icon(Icons.people_alt_outlined),
+                      onPressed: (){
+                        Get.to(AboutPage());
+                      }
+                    ),
+                  ],
+                ),
+              ),
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    LogoApp(),
+                    const SizedBox(height: 10),
+                    ButtonInicial(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
